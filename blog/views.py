7bc69e7271f5937db16aa5f73304a1b1bef533b0
kdmodngmai <kdmdongmai@gmail.com>
2021-08-30 12:19:21 +0900

    list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request, 'blog/index.html', context)
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/detail.html', {'post':post})
