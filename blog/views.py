360361b46c1c36e86b6ef4f744da2de83f5a2b0a
kdmodngmai <kdmdongmai@gmail.com>
2021-08-31 10:06:50 +0900

    before bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
+class PostList(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all()
-    context = {'posts':posts}
-    return render(request, 'blog/index.html', context)
+# def index(request):
+#     posts = Post.objects.all()
+#     context = {'posts':posts}
+#     return render(request, 'blog/post_list.html', context)
 
-def detail(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/detail.html', {'post':post})
+# def detail(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     context= {'object':post, 'post':post}
+#     return render(request, 'blog/post_detail.html', context)
+
